b540a92810f48b1c0ce6ca7af85414e662d3ea40
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-03-20 00:06:31 +0800

    [API]

--- application/api/home/Index.php
+++ application/api/home/Index.php
@@ -336,7 +336,7 @@ class Index
         }
         $pmap['memberid'] = $member['memberid'];
         $pmap['behavior_type'] = 0;
-        $member['point'] = db('member_point')->where($pmap)->sum('point');
+        $member['points'] = db('member_point')->where($pmap)->sum('point');
         //返回信息
         $data = [
             'code'=>'1',
