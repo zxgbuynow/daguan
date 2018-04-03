11674ec3a37e389e5a09f0352946bba12d1b013a
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-04-03 10:03:06 +0800

    [pay]

--- public/static/counsellor/js/app.js
+++ public/static/counsellor/js/app.js
@@ -3,6 +3,7 @@ var error_page = '<div class="nodata-wrapper"><div class="nodata-layout"><div cl
 var nodata_el_active = '<div class="nodata-wrapper nodata-wrapper-active"><div class="nodata-layout"><div class="nodata-icon"><i class="bbc-icon bbc-icon-history-gap"></i></div><div class="nodata-tip">活动尚未开启，敬请期待</div></div></div>';
 document.addEventListener("plusready", onPlusReady, false);
 
+
 function onPlusReady() {
   document.addEventListener("netchange", onNetChange, false);
 }
@@ -1033,6 +1034,38 @@ $(function() {
     return sign + curr.join(spec.thousands_sep) + spec.dec_point + part;
   });
 
+  template.helper('$getDateDiff', function(dateTimeStamp) {
+    var minute = 1000 * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var halfamonth = day * 15;
+    var month = day * 30;
+    var now = new Date().getTime();
+    var diffValue =dateTimeStamp*1000 - now;
+    if(diffValue < 0){return;}
+    var monthC =diffValue/month;
+    var weekC =diffValue/(7*day);
+    var dayC =diffValue/day;
+    var hourC =diffValue/hour;
+    var minC =diffValue/minute;
+    if(monthC>=1){
+      result="" + parseInt(monthC) + "月后";
+    }
+    else if(weekC>=1){
+      result="" + parseInt(weekC) + "周后";
+    }
+    else if(dayC>=1){
+      result=""+ parseInt(dayC) +"天前";
+    }
+    else if(hourC>=1){
+      result=""+ parseInt(hourC) +"小时后";
+    }
+    else if(minC>=1){
+      result=""+ parseInt(minC) +"分钟后";
+    }else
+    result="刚刚";
+    return result;
+  });
   //价格转换正整数
   template.helper('$int_price', function(num) {
     return Math.round(num);
