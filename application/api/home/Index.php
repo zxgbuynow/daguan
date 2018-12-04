f5c57bdf047e9ffa23fdac77273e8ce0ccc58579
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-12-04 11:03:35 +0800

    []

--- application/api/home/Index.php
+++ application/api/home/Index.php
@@ -4937,21 +4937,30 @@ class Index
 
         // $times = array('9:00~10:00','10:00~11:00','11:00~12:00','12:00~13:00','13:00~14:00','14:00~15:00','15:00~16:00','16:00~17:00','17:00~18:00','18:00~19:00','19:00~20:00','20:00~21:00');
         $times = array('0:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00');
-        $timearrr = array('0:00~6:00'=>array('0:00','1:00','2:00','3:00','4:00','5:00','6:00'),'6:00~18:00'=>array('6:00','7:00','8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00'),'18:00~24:00'=>array('18:00','19:00','20:00','21:00','22:00','23:00'));
+        $timearrr = array('0:00~5:00'=>array('0:00','1:00','2:00','3:00','4:00','5:00'),'6:00~18:00'=>array('6:00','7:00','8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00'),'19:00~24:00'=>array('19:00','20:00','21:00','22:00','23:00'));
 
         $rangearr = $timearrr[$rangetm];
         $arr = [];
-        foreach ($times as $key => $value) {
+        // foreach ($times as $key => $value) {
+        //     $hour = $value;
+        //     $data['ondatetime'] = strtotime($day.' '.$hour);
+        //     $data['memberid'] = $account;
+        //     if (!db('connsellor_ondate')->where($data)->find()&&in_array($value, $rangearr)) {
+        //         $arr[] = $data;
+        //         db('connsellor_ondate')->insert($data);
+        //     }
+            
+        // }
+
+        foreach ($rangearr as $key => $value) {
             $hour = $value;
             $data['ondatetime'] = strtotime($day.' '.$hour);
             $data['memberid'] = $account;
-            if (!db('connsellor_ondate')->where($data)->find()&&in_array($value, $rangearr)) {
-                $arr[] = $data;
+            if (!db('connsellor_ondate')->where($data)->find()) {
                 db('connsellor_ondate')->insert($data);
             }
             
         }
-
 
         //返回信息
         $data = [
