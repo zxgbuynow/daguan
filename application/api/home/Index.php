c71204f9994d54333893ffc81b3e0bb19b4c0a9e
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-12-07 15:38:49 +0800

    [

--- application/api/home/Index.php
+++ application/api/home/Index.php
@@ -3068,7 +3068,8 @@ class Index
             }
             
             //msg
-            $news = db('hx_msg')->where($map)->order('id DESC')->find();
+            $smsg['tag'] = $value['tag'];
+            $news = db('hx_msg')->where($smsg)->order('id DESC')->find();
             $res[$key]['nmsg'] = $news['msg'];
 
             //time
