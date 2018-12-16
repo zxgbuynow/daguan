d7cdf1df03161ff17aacd2bca5329be7c612e3bb
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-12-16 12:26:14 +0800

    []

--- application/api/home/Index.php
+++ application/api/home/Index.php
@@ -2020,12 +2020,21 @@ class Index
         $save['end_time'] = strtotime($start_time)+60*60;
         $save['tid'] = $tid;
 
-        $save['title'] = db('trade')->where('id',$tid)->column('title')[0];
+        $save['title'] = db('trade')->where('id',$tid)->value('title');
         $save['descrption'] = db('trade')->where('id',$tid)->column('title')[0];
 
         if (!db('calendar')->insert($save)) {
             $this->error('保存失败！');
         }
+
+        //发送预约短信
+        $content = $save['descrption'];
+        $uid = db('trade')->where('id',$tid)->value('memberid');
+        $mobile = db('member')->where(['id'=>$uid])->value('mobile');
+        $sj = strtotime($start_time);
+        $mobile = 18321271831;
+        $this->sendadvicemsg($mobile,$content,$sj);
+
         //更新预约表状态
         db('connsellor_ondate')->where(['memberid'=>$account,'ondatetime'=>strtotime($start_time)])->update(['status'=>1]);
         //返回信息
@@ -6216,6 +6225,46 @@ class Index
                 return false;  
             }  
         }  
+    }
+
+    /**
+     * [sendmsg description]
+     * @param  [type] $mobile [description]
+     * @return [type]         [description]
+     */
+    public function sendadvicemsg($mobile,$content,$sj)
+    {
+        $apikey = "8df6ed7129c50581eecdf1e875edbaa3"; 
+
+        $text = '【大观心理】温馨提示：您有新的心理咨询预约：'.$content."，时间（".date('Y-m-d H:i',$sj)."）"; 
+
+        // error_log($text,3,'/home/wwwroot/daguan/mobile.log');
+        $ch = curl_init();
+ 
+         /* 设置验证方式 */
+         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:text/plain;charset=utf-8',
+             'Content-Type:application/x-www-form-urlencoded', 'charset=utf-8'));
+         /* 设置返回结果为流 */
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         
+         /* 设置超时时间*/
+         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+         
+         /* 设置通信方式 */
+         curl_setopt($ch, CURLOPT_POST, 1);
+         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+         
+         // 发送短信
+         $data = array('text'=>$text,'apikey'=>$apikey,'mobile'=>$mobile);
+         $json_data = $this->send($ch,$data);
+         // error_log($json_data,3,'/home/wwwroot/daguan/sendmsg.log');
+         $array = json_decode($json_data,true); 
+         // print_r($array);exit; 
+         if ($array['code']==0) {
+            return true;
+         }else{
+            return false;
+         }
     }
 
     /**
